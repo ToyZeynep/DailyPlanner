d34295acd2c4a71870af9876685bcf0ab43ea011
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-09 13:34:30 +0300

    Create Plan List Worker and add necessary methods

--- DailyPlanner/DailyPlanner/ManagerProtocols/CoreDataManager.swift
+++ DailyPlanner/DailyPlanner/ManagerProtocols/CoreDataManager.swift
@@ -8,9 +8,10 @@
 import Foundation
 import CoreData
 
-protocol DataManagerProtocol: AnyObject{
+protocol CoreDataManagerProtocol: AnyObject{
     func addPlan(completionTime: Date, name: String, details: String, isComplete: Bool, priority: String , willNotify: Bool , category: String)
     func removePlan(object: Plan?)
     func getPlanList(completion: @escaping ((Result<[Plan], Error>) -> Void))
-    func updatePlan(object: Plan)
+    func updateIsComplete(object: Plan)
+    func updateWillNotify(object: Plan)
 }
--- DailyPlanner/DailyPlanner/Presentation/PlanListView/PlanListWorker.swift
+++ DailyPlanner/DailyPlanner/Presentation/PlanListView/PlanListWorker.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import Foundation
+import UIKit
+import CoreData
+
+final class PlanListWorker: CoreDataManagerProtocol  {
+    func addPlan(completionTime: Date, name: String, details: String, isComplete: Bool, priority: String, willNotify: Bool, category: String) {
+    }
+    
+    func removePlan(object: Plan?) {
+        let  managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            print(object?.name as Any)
+        managedContext.delete(object!)
+        do {
+            try managedContext.save()
+            print("saved")
+        } catch {
+            print("error")
+        }
+    }
+    
+    func updateIsComplete(object: Plan) {
+        let  managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if object.isComplete == true{
+            object.isComplete = false
+        }else {
+            object.isComplete = true
+        }
+        print(object.isComplete)
+        do {
+            try managedContext.save()
+            print("saved")
+        } catch {
+            print("error")
+        }
+    }
+    
+    func getPlanList(completion: @escaping ((Result<[Plan], Error>) -> Void)) {
+        do {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            var models = try context.fetch(Plan.fetchRequest())
+            completion(.success(models))
+        } catch {
+            completion(.failure(error))
+        }
+        
+    }
+    
+    func updateWillNotify(object: Plan){
+        
+        let  managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+       
+        if object.willNotify == true{
+            object.willNotify = false
+        }else {
+            object.willNotify = true
+        }
+   
+        do {
+            try managedContext.save()
+            print("saved")
+        } catch {
+            print("error")
+        }
+    }
+  
+
+}
