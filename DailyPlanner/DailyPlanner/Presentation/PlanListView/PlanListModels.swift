293bee6e732e120c59ef3e8baaa18df08ed52f6e
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-09 13:43:19 +0300

    Create Plan List Model

--- DailyPlanner/DailyPlanner/Presentation/PlanListView/PlanListModels.swift
+++ DailyPlanner/DailyPlanner/Presentation/PlanListView/PlanListModels.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import CoreData
+enum PlanList {
+    
+    enum Fetch {
+        
+        struct Request {
+        }
+        
+        struct Response {
+            var planList: [Plan]
+        }
+        
+        struct ViewModel {
+            var planList: [PlanList.Fetch.ViewModel.Plan?]
+
+            struct Plan {
+                var name: String?
+                var completionTime: Date?
+                var isComplete: Bool?
+                var details: String?
+                var priority: String?
+                var willNotify: Bool?
+                var category: String?
+            }
+        }
+    }
+}
+
