94bde5e3e0d9a133c8c5c53d9ec437526fcadd01
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-09 17:33:18 +0300

    fix splash view bugs

--- DailyPlanner/DailyPlanner/Presentation/SplashView/SplashViewController.swift
+++ DailyPlanner/DailyPlanner/Presentation/SplashView/SplashViewController.swift
@@ -14,9 +14,11 @@ class SplashViewController: UIViewController {
     @IBOutlet weak var splashImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        splashImageView.tintColor = .systemPurple
+    
         splashImageView.loadGif(asset: "splash")
         splashImageView.translatesAutoresizingMaskIntoConstraints = false
+        splashImageView.contentMode = .scaleAspectFit
+        
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [self] in
             let storyBoard = UIStoryboard(name: "PlanList", bundle: nil)
